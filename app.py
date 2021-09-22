f49e4dee87e7a224c09387a7ee1fe9098bdc2b1f
faithchep <faithchepkoech40@gmail.com>
2021-09-22 14:28:10 +0300

    fetch forms

--- app.py
+++ app.py
@@ -20,5 +20,20 @@ def data():
     return resp.json()
 
 
+@app.route('/forms')
+def forms():
+    url = "https://hiskenya.org/api/dataEntryForms/"
+
+    headers = CaseInsensitiveDict()
+    headers["Content-Type"] = "application/json"
+    headers["Authorization"] = "Basic U2luamlyaTpPdHdlcm85Ni4="
+    headers["Cookie"] = "JSESSIONID=7566DEA3E3668BBB6FAB103EE05499F7"
+
+    resp = requests.get(url, headers=headers)
+
+    print(resp.text)
+    return resp.json()
+
+
 if __name__ == '__main__':
     app.run()
